abdb068ba11ac3af7f80ee6f38af28e4941571b4
Stephen Sullivan <sjsullivan7@gmail.com>
2017-12-24 11:14:12 -0500

    Part2 for Day18

--- src/main/java/com/github/ssullivan/day18/DuetProgramPart2.java
+++ src/main/java/com/github/ssullivan/day18/DuetProgramPart2.java
@@ -1,25 +1,38 @@
 package com.github.ssullivan.day18;
 
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 public class DuetProgramPart2 extends DuetProgram {
-
+  private int id;
   private BlockingQueue<Long> inputQueue;
   private BlockingQueue<Long> outputQueue;
+  private Long counter = 0L;
 
-  public DuetProgramPart2(final BlockingQueue<Long> outputQueue) {
-    this.inputQueue = new LinkedBlockingQueue<>();
+  public DuetProgramPart2(int id,
+      BlockingQueue<Long> inputQueue,
+      BlockingQueue<Long> outputQueue) {
+    this.id = id;
+    this.inputQueue = inputQueue;
     this.outputQueue = outputQueue;
+
+    set("p", (long) id);
+  }
+
+  public Long getCounter() {
+    return counter;
   }
 
   @Override
   protected void snd(String x) {
-    this.outputQueue.offer(get(x));
+   if (this.outputQueue.offer(get(x))) {
+     this.counter++;
+   }
   }
 
   @Override
   protected void rcv(String x) throws InterruptedException {
+    if (id == 1)
+      System.out.println("Part 2 [id: " + id + "] " + this.counter);
    set(x, this.inputQueue.take());
   }
 }
