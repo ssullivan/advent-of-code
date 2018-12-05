103086068178a7753656f721004875ce1c2b5225
sjsullivan7 <sjsullivan7@gmail.com>
2018-12-05 12:23:47 -0500

    fixed intersection code

--- 2018/day3/part1.py
+++ 2018/day3/part1.py
@@ -10,6 +10,12 @@ class Rect:
   def left(self) -> int:
     return self._left
 
+  def right(self) -> int:
+    return self._left + self._width
+
+  def bottom(self) -> int:
+    return self._top + self._height
+
   def top(self) -> int:
     return self._top
 
@@ -26,6 +32,8 @@ class Claim:
     self._rect = Rect(left, top, width, height)
 
   def __init__(self, raw_claim: str):
+    # #{id} @ {left},{top}: {width}x{height}
+    self._raw = raw_claim
     claim_pattern = re.compile("^(#\d+)\s+@\s+(\d+),(\d+):\s+(\d+)x(\d+)$")
     match = claim_pattern.match(raw_claim)
     self._id = match.group(1)
@@ -34,16 +42,19 @@ class Claim:
   def rect(self) -> Rect:
     return self._rect
 
+  def __str__(self):
+    return self._raw
+
 
 def intersect_range(value: int, min: int, max: int) -> bool:
   return value >= min and value <= max
 
-def intersect_rect(lhs: Rect, rhs: Rect) -> bool:
-  left_overlap = intersect_range(lhs.left(), rhs.left(), rhs.left() + rhs.width()) or \
-    intersect_range(lhs.left(), lhs.left(), lhs.left() + lhs.width())
-  right_overlap = intersect_range(lhs.top(), rhs.top(), rhs.top() + rhs.height()) or \
-                  intersect_range(lhs.top(), lhs.top(), lhs.top() + lhs.height())
-  return left_overlap and right_overlap
+def intersect_rect(a: Rect, b: Rect) -> bool:
+  return intersect_range(b.left(), a.left(), a.right()) or \
+      intersect_range(a.left(), b.left(), b.right()) or \
+      intersect_range(b.top(), a.top(), a.bottom()) or \
+      intersect_range(a.top(), b.top(), b.bottom())
+
 
 def intersect(lhs: Claim, rhs: Claim) -> bool:
   return intersect_rect(lhs.rect(), rhs.rect())
@@ -54,4 +65,12 @@ if __name__ == "__main__":
   with open("input.txt", "r") as f:
     claims = [Claim(line.strip()) for line in f]
 
-  print()
+    for i in range(0, len(claims)):
+      for j in range(0, len(claims)):
+        if i == j:
+          continue
+
+        if intersect(claims[i], claims[j]):
+          print(f'intersect {claims[i]}, {claims[j]}')
+        else:
+          print(f'dont intersect {claims[i]}, {claims[j]}')
