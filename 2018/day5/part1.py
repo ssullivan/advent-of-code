5bebed52621f6eb651f289a1003904b4c6e0057b
sjsullivan7 <sjsullivan7@gmail.com>
2018-12-05 23:04:16 -0500

    it works

--- 2018/day5/part1.py
+++ 2018/day5/part1.py
@@ -3,37 +3,31 @@ if __name__ == "__main__":
     original_input_polymer = f.read().strip()
 
     original = list(original_input_polymer)
-    ptr = 0
+
     changes = 0
     while True:
-      if ptr + 1 >= len(original):
-        if changes <= 0:
+      starting_len = len(original)
+      ptr = 0
+
+      while True:
+        if ptr + 1 >= starting_len:
           break
-        ptr = 0
-        changes = 0
+        first = str(original[ptr])
+        second = str(original[ptr + 1])
 
-      first = original[ptr]
-      second = original[ptr + 1]
-
-      # Cover case aA or Aa
-      if not(first.upper() == second or \
-             second.upper() == first):
+        # make sure they are the same 'type'
+        if first.lower() == second.lower():
+          if (first.islower() and second.isupper()) or (first.isupper() and second.islower()):
+            original.pop(ptr)
+            original.pop(ptr)
+            break
         ptr += 1
-      else:
-        original.pop(ptr)
-        original.pop(ptr)
-        ptr = 0
-        changes += 1
-
-      if len(original) % 10 == 0:
-        print(f'{len(original)},{ptr}')
-
-
-
-
-
+      ending_len = len(original)
+      if ending_len == starting_len:
+        break
+      print(f'Starting: {starting_len}, Ending: {ending_len}')
+    print(len(original))
     print("".join(original))
-
 
 
 
