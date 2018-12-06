ca01b33849121abed0788ac69c0be4fadb8e2833
sjsullivan7 <sjsullivan7@gmail.com>
2018-12-05 22:45:47 -0500

    exit conditions. fix incr

--- 2018/day5/part1.py
+++ 2018/day5/part1.py
@@ -1,29 +1,36 @@
 if __name__ == "__main__":
   with open("input.txt", "r") as f:
     original_input_polymer = f.read().strip()
+
     original = list(original_input_polymer)
     ptr = 0
-    counter = 0
+    changes = 0
     while True:
+      if ptr + 1 >= len(original):
+        if changes <= 0:
+          break
+        ptr = 0
+        changes = 0
 
       first = original[ptr]
       second = original[ptr + 1]
 
       # Cover case aA or Aa
       if not(first.upper() == second or \
-        second.upper() == first):
-        ptr += 2
+             second.upper() == first):
+        ptr += 1
       else:
         original.pop(ptr)
         original.pop(ptr)
         ptr = 0
-      print(f'{len(original)},{ptr}')
+        changes += 1
 
-      if ptr + 1 >= len(original):
-        break
+      if len(original) % 10 == 0:
+        print(f'{len(original)},{ptr}')
 
-      if counter >= len(original):
-        counter = 0
+
+
+
 
     print("".join(original))
 
