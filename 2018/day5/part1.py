6245c501849cd5d18d3ce4589fa3815e9b079e04
sjsullivan7 <sjsullivan7@gmail.com>
2018-12-05 22:26:30 -0500

    perf changes

--- 2018/day5/part1.py
+++ 2018/day5/part1.py
@@ -1,21 +1,21 @@
 if __name__ == "__main__":
-  with open("sample.txt", "r") as f:
+  with open("input.txt", "r") as f:
     original_input_polymer = f.read().strip()
     original = list(original_input_polymer)
     ptr = 0
     counter = 0
     while True:
-      first = str(original.pop(ptr))
-      second = str(original.pop(ptr))
+
+      first = original[ptr]
+      second = original[ptr + 1]
 
       # Cover case aA or Aa
       if not(first.upper() == second or \
         second.upper() == first):
-        original.insert(ptr, second)
-        original.insert(ptr, first)
         ptr += 2
       else:
-       # print(f'removed {first}{second}')
+        original.pop(ptr)
+        original.pop(ptr)
         ptr = 0
       print(f'{len(original)},{ptr}')
 
